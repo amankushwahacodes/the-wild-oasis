d25e5aba7854b5572e2ed7fdf57bca32b4bf4dcf
Aman Kushwaha <amankushwaha7284@gmail.com>
2025-12-11 23:29:21 +0530

    Bookings

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,6 +5,7 @@ import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 
 function CabinTable() {
@@ -12,6 +13,8 @@ function CabinTable() {
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
+
 
   const filterValue = searchParams.get('discount') || "all";
   
